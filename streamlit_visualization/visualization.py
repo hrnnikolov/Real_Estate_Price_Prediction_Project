044ed3102647c38f2883306904ff7a0f5378029a
Hristo Nikolov <100585456+hrnnikolov@users.noreply.github.com>
2023-12-07 12:45:48 +0200

    Add files via upload

--- streamlit_visualization/visualization.py
+++ streamlit_visualization/visualization.py
@@ -1,34 +1,14 @@
 import streamlit as st
-import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import json
-import pickle
 import joblib
-#import testingz
 import os
-
-path = os.path.dirname(__file__)
-my_data = path+'/columns.json'
-my_model = path+'/lr_model.joblib'
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-st.title('Real_Estate_Price_Prediction_Project')
-
-with open(my_data, 'r') as f:
-    data = json.load(f)['data_columns']
-    location_names= data
-
-with open(my_model, 'rb') as f:
-    model1 = joblib.load(f)
+import base64
 
 
 def predict_price(location, sqft, bath, bedrooms):
     try:
-        loc_index = location_names.index(location.lower())
+        loc_index = location_names.index(location)
     except:
         loc_index  = -1
 
@@ -42,25 +22,61 @@ def predict_price(location, sqft, bath, bedrooms):
 
     return str(model1.predict([x])[0])
 
-st.write(predict_price('2nd Stage Nagarbhavi', 1000, 5, 2))
 
-if st.button('Goals Diagram'):
-    # Data set
-    height = list(df_onlyplayers['Gls'])
-    bars = list(df_onlyplayers['Player'])
-    y_pos = np.arange(len(bars))
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
 
-    # Basic bar plot
-    plt.bar(y_pos, height, color=(0.2, 0.4, 0.6, 0.6))
-    plt.xticks(y_pos, bars, rotation=90)
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
 
-    # Custom Axis title
-    # plt.xlabel('Players', fontweight='bold', color = 'orange', fontsize='17', horizontalalignment='center')
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
 
-    # Show the graph
-    st.pyplot()
 
-# In[ ]:
+path = os.path.dirname(__file__)
+my_data = path+'/columns_upper.json'
+my_model = path+'/lr_model.joblib'
+background_png = path+'/background.png'
+
+#set_bg_hack(background_png)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title('Real Estate Price Prediction')
+
+with open(my_data, 'r') as f:
+    location_names = json.load(f)['data_columns']
+    #location_names= data
+
+with open(my_model, 'rb') as f:
+    model1 = joblib.load(f)
+
+# st.markdown("<span style='color:red'>Location</span>",
+#              unsafe_allow_html=True)
+
+location = st.selectbox('Location', location_names[3:])
+area = st.text_input('Area in square feet')
+baths = st.text_input('How many baths')
+bedrooms = st.text_input('How many rooms')
+
+if st.button('Calculate price'):
+    #st.write(predict_price(location_names, area, baths, bedrooms))
+    st.markdown(f'The ***price*** for a ***{area}*** sqft apartment in ***{location}*** with ***{bedrooms}*** bedrooms and ***{baths}***'
+                f' bathrooms will be ***{float(predict_price(location, area, baths, bedrooms))*1200:.2f}$***')
 
 
 
